881ce866171f244a4e0c105db74b491d78bb2b87
6netic <ebresler@live.fr>
2020-12-14 18:18:08 +0100

    Creating empty structure

--- targetapp/views.py
+++ targetapp/views.py
@@ -11,6 +11,10 @@ def index():
 	return render_template("index.html")
 
 
+@app.route("/ten")
+def tenmeters():
+	""" View for 10 meters calculation """
 
+	return render_template("tenmeters.html")
 
 
